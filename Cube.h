a58ac0d075bb7596f52df68ed8a5f04a92d606db
Abanob Ayoub <101833405+aayoub3@users.noreply.github.com>
2023-04-20 23:21:28 -0700

    Create Cube.h

---
+++ Cube.h
@@ -0,0 +1,30 @@
+// COPYRIGHTS.© ABANOB AYOUB
+// Description: Class header file
+
+#pragma once
+#include<string>
+using namespace std;
+class Cube
+{
+private:
+	int side;
+	string SingleSpace;
+	string SingleStar;
+	int spacesBefore;
+	int spacesOnRight;
+	int spacesBetween;
+
+public:
+	Cube();
+	int getSide(); //getter
+	void setSide(int sidePar); //setter
+	void drawFirstLine(int userInputPar);
+	void drawUpperHalf(int userInputPar);
+	void drawMidLine(int userInputPar);
+	void drawButtomHalf(int userInputPar);
+	void drawLastLine(int userInputPar);
+	void displayCube(int userInputPar);
+};
+
+// COPYRIGHTS.© ABANOB AYOUB
+// COPYRIGHTS.© ABANOB AYOUB
